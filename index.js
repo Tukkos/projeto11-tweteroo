0c86f136fa78d44f5c499fce06a3507f1c5d189a
Tukkos <jpfcastro@id.uff.br>
2022-08-26 09:34:10 -0300

    GET working without 10 tweets limit

--- index.js
+++ index.js
@@ -2,17 +2,43 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const login = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+];
+const tweets = [
+    {
+	    username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	}
+];
 
 app.use(cors());
 
 app.post("/sign-up", (req, res) =>{
-    
+    login = req.body;
+    res.send("OK");
 })
 
-app.get("/", (req, res) => {
-    res.send();
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweet.avatar = login.avatar;
+    tweets.push(tweet);
+    res.send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
 });
 
 app.listen(5000, () => console.log("Listen on 5000"))
 
-// http://localhost:4000/
+// http://localhost:5000/sign-up
