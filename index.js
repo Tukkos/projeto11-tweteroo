b3a1014195e755b27b1d9a9e09d9d685f4ffbd08
Tukkos <jpfcastro@id.uff.br>
2022-08-26 10:40:46 -0300

    users and tweets format changed, now GET /tweets serach for the avatar image

--- index.js
+++ index.js
@@ -5,37 +5,56 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let login = {};
+let users = [
+    {
+        username: "Gunter",
+		avatar: "https://img.elo7.com.br/product/600x380/3EA7E44/gunter-hora-de-aventura-festa-cartoon-network.jpg" 
+    },
+    {
+        username: "Lady Rainicorn",
+		avatar: "https://i.pinimg.com/736x/0e/b9/c6/0eb9c654879b90bc99e8642992d1624e.jpg"
+    },
+    {
+        username: "Jake",
+		avatar: "https://static3.tcdn.com.br/img/img_prod/906555/90_camiseta_hora_de_aventura_jake_o_cachorro_141_3_7a8466862e0c230ff5ff83e65fbf365a.jpg"
+    }
+];
 let tweets = [
     {
-	    username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
+	    username: "Lady Rainicorn",
+	    tweet: "안녕! "
 	},
     {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	}
+		username: "Gunter",
+	    tweet: "Wenk wenk!"
+	},
+    {
+        username: "Jake",
+	    tweet: "Um belo dia pra dar um cochilo."
+    }
 ];
 
 app.use(cors());
 
 app.post("/sign-up", (req, res) =>{
-    login = req.body;
+    users.push(req.body);
     res.send("OK");
 })
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body;
-    tweet.avatar = login.avatar;
-    tweets.push(tweet);
+    tweets.push(req.body);
     res.send("OK");
 })
 
 app.get("/tweets", (req, res) => {
-    let timeline = tweets.slice(-10);
-    res.send(timeline);
+    for (let i = 0; i < tweets.length; i++) {
+        for (let j = 0; j < users.length; j++) {
+            if(tweets[i].username === users[j].username) {
+                tweets[i].avatar = users[j].avatar;
+            }
+        }
+    };
+    res.send(tweets.slice(-10));
 });
 
 app.listen(5000, () => console.log("Listen on 5000"))
